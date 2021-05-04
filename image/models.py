0bec98f926fb45d94399b8a024149e74e9b249c9
Lewis Jang <lewis.aedes@gmail.com>
2021-05-04 15:54:47 +0800

    add entity to str

--- image/models.py
+++ image/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Entity(models.Model):
     image = models.ImageField(upload_to='upload_images')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + " - " + str(self.created_at)
